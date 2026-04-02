6ca694f247e1ae784e6c354bf63b62dbe93e77ac
Tiago Santana <tiagosantana@pactosolucoes.com.br>
2026-04-01 21:35:04 -0300

    incluir allure

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,44 +4,24 @@ import allureWriter from '@shelex/cypress-allure-plugin/writer'
 export default defineConfig({
     e2e: {
         baseUrl: 'https://blogdoagi.com.br',
-
         viewportWidth: 1280,
         viewportHeight: 720,
-
         defaultCommandTimeout: 10000,
         pageLoadTimeout: 30000,
-
         video: false,
         screenshotOnRunFailure: true,
-
         screenshotsFolder: 'cypress/screenshots',
-
         specPattern: 'cypress/e2e/**/*.cy.ts',
         supportFile: 'cypress/support/e2e.ts',
 
-        // 🔥 ESSENCIAL PRO ALLURE
         setupNodeEvents(on, config) {
             allureWriter(on, config)
-
-            // (opcional, mas bom pra debug)
-            on('task', {
-                log(message) {
-                    console.log(message)
-                    return null
-                }
-            })
-
             return config
-        }
+        },
     },
 
-
-    retries: {
-        runMode: 1,
-        openMode: 0
-    },
-
-
-    chromeWebSecurity: false
+    env: {
+        allure: true,
+        allureResultsPath: "allure-results"
+    }
 })
-
